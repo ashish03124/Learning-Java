43696df751c8bf8e24a113b122533e4970a7a27b
Ashish Garud <142095358+ashish03124@users.noreply.github.com>
2025-01-31 23:35:50 +0530

    History of java

---
+++ HistoryOfJava.java
@@ -0,0 +1,18 @@
+/*
+ * HISSTORY OF THE JAVA
+ * ->green team
+ * ->deveeloped by james gosling,1995
+ * ->greentalk,oak,java
+ * ->released in 1995
+ * ->High security
+ * ->size of primitive types is fixed
+ * ->slower than compiled lang like cpp due to its interpretation
+ * ->CASE sesitive
+ * ->INPUT is taken under scanner class
+ * ->JVM--java virtual machine used while exicution
+ * ->JRE--java runtime environment used for classes,liabraries
+ * ->JDK--java DDevelopment kit used to develope applications
+ * 
+ * 
+ * 
+ */
